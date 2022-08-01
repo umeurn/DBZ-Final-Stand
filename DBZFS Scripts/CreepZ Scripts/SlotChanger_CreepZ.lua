daf116cec5d669f30ee1eea1e81c3bb1d851d508
Dev <103299052+TheS1mpleDev@users.noreply.github.com>
2022-08-01 05:43:16 -0500

    Delete DBZFS Scripts directory

--- DBZFS Scripts/CreepZ Scripts/SlotChanger_CreepZ.lua
+++
@@ -1,5 +0,0 @@
--- CreepZ Slot Changer
--- Type ~slot to change slots
-getgenv().prefix="~";
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/llCreepZll/llCreepZll/DBZFS_Scripts/slotchanger", true))();
--- DBZFS Scripts/CreepZ Scripts/nouigodaura.lua
+++
@@ -1,7 +0,0 @@
-(game:GetService("RunService")).RenderStepped:Connect(function()
-	for i, v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-		if v.Name:find("Ui") then
-			v:Destroy();
-		end;
-	end;
-end);
