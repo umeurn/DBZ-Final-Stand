90874d617c2ccc21f0f73d11443a20efec7f5d4c
Demon <103299052+y2kstar@users.noreply.github.com>
2023-09-04 14:03:53 -0500

    Delete DBZFS Scripts/CreepZ Scripts/Forcefieldremove.lua

--- DBZFS Scripts/CreepZ Scripts/Forcefieldremove.lua
+++
@@ -1,7 +0,0 @@
-game:GetService("RunService").RenderStepped:Connect(function()
-    for _, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
-        if v.ClassName == "ForceField" then 
-            v:Destroy(); 
-        end;
-    end;
-end);
