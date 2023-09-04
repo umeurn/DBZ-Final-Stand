913c6b78760a920342eab18d04b2df46d7b29149
Demon <103299052+y2kstar@users.noreply.github.com>
2023-09-04 14:03:58 -0500

    Delete DBZFS Scripts/CreepZ Scripts/Infinite_2x_exp.lua

--- DBZFS Scripts/CreepZ Scripts/Infinite_2x_exp.lua
+++
@@ -1,5 +0,0 @@
-game:GetService("RunService").Stepped:Connect(function()
-    if game.Players.LocalPlayer.Character:FindFirstChild("True") then
-        game.Players.LocalPlayer.Character:FindFirstChild("True"):Destroy();
-    end;
-end);
